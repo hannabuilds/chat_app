de1de75c9e8daaa357b3be854c15d6942b1e76fb
Hannah Joy Hotricano <hannahotricano@gmail.com>
2025-06-18 23:44:08 -0700

    set up frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import SettingsPage from './pages/SettingsPage';
+import ProfilePage from './pages/ProfilePage';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useAuthStore } from './store/useAuthStore.js';
+import {useEffect} from 'react';
+import { Routes, Route } from 'react-router-dom'; 
+import { Loader } from 'lucide-react';
+
+
+const App = () => {
+  const {authUser, checkAuth} = useAuthStore()
+
+  useEffect(() => {checkAuth ()}, {checkAuth});
+   
+  console.log({authUser});
+
+  if(isCheckingAuth && !authUser) 
+    return(
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="sixe-10 animate-spin"/>
+    </div>
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div >
+      <Navbar />
 
-export default App
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login"/>} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+
+      </Routes>
+  </div>
+  ) 
+};
+
+export default App;
