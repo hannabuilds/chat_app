668961875f45a8fce00ee883656a851e2ad17220
Hannah Joy Hotricano <hannahotricano@gmail.com>
2025-06-21 23:40:37 -0700

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import { useAuthStore } from './store/useAuthStore';
 import {useEffect} from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom'; 
 import { Loader } from 'lucide-react';
+import { Toaster } from "react-hot-toast";
 
 
 const App = () => {
@@ -37,6 +38,8 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+
+      <Toaster />
   </div>
   ); 
 };
