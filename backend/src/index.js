86d0edfe5fc6fffc9b50bae0d9d2eb7c501fd2c3
Hannah Joy Hotricano <hannahotricano@gmail.com>
2025-06-22 23:45:46 -0700

    add ProfilePage and profile update function

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,12 +14,15 @@ const app = express();
 
 const PORT = process.env.PORT
 
-app.use(express.json());
+
+app.use(express.json({ limit: "5mb"}));
+app.use(express.urlencoded({limit: "5mb", extended: true}));
 app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
 }));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
