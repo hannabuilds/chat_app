4f90fe54cd21d99ad37b266213248e80d3656a3d
Hannah Joy Hotricano <hannahotricano@gmail.com>
2025-06-27 17:17:27 -0700

    .

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -71,7 +71,7 @@ const ChatContainer = () => {
                 {formatMessageTime(message.createdAt)}
               </time>
             </div>
-            <div className="chat-bubble flex flex-col">
+            <div className={`chat-bubble flex flex-col ${message.senderId === authUser._id ? "bg-primary text-primary-content" : ""}`} >
               {message.image && (
                 <img
                   src={message.image}
